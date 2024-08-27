286a57c4b2f31284fa1de7a96305b3ca9cb816ab
Jihan <jihanahmed41@gmail.com>
2024-08-27 07:16:26 -0700

    8-all

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,7 +2,7 @@
 """ a Python function that lists all documents in a collection"""
 
 
-def list_all(mongo_collection: Collection) -> List[Dict[str, Any]]:
+def list_all(mongo_collection):
     """
      List all documents in Python
     """
