3c4d6a26dbc2a79f1014b704dacd07628adda15b
Jihan <jihanahmed41@gmail.com>
2024-08-27 07:15:07 -0700

    8-all

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 """ a Python function that lists all documents in a collection"""
-from typing import List, Dict, Any
-from pymongo.collection import Collection  # type: ignore
 
 
 def list_all(mongo_collection: Collection) -> List[Dict[str, Any]]:
@@ -11,4 +9,4 @@ def list_all(mongo_collection: Collection) -> List[Dict[str, Any]]:
     if mongo_collection.count_documents({}) == 0:
         return []
     docs = mongo_collection.find()
-    return list(docs)
+    return docs
