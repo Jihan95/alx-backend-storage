8cdd72bfaec467a58bdf5c9eb3b99465c06b570c
Jihan <jihanahmed41@gmail.com>
2024-08-28 11:54:11 +0000

    initial commit

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Writing strings to Redis
+"""
+import redis
+from uuid import uuid1
+from typing import Union
+
+class Cache:
+    """
+    class represents cache memory
+    """
+    def __init__(self):
+        """
+        inictantiation method
+        """
+        self._redis = redis.Redis(host='localhost', port=6379, db=0)
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        store the input data in Redis using the random key and return the key
+        """
+        key = str(uuid1())
+        self._redis.set(key, data)
+        return key
